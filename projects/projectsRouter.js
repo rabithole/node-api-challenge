7d6fcecd66844087f6506d815dae865bae860371
Robert Kulp <pedalinon@gmail.com>
2020-03-20 20:45:44 -0700

    MVP, I think!

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -28,8 +28,8 @@ router.post('/', (req, res) => {
   })
 });
 
-router.post('/:id/action', (req, res) => {
-	console.log('action insertion:', req.params);
+router.post('/:id/action', validateId, (req, res) => {
+	// console.log('action insertion:', req.params);
 	
 	Action.insert( { project_id: req.params.id, description: req.body.description, notes: req.body.notes } )
 	.then(action => {
@@ -41,15 +41,35 @@ router.post('/:id/action', (req, res) => {
 });
 
 router.get('/:id/action', (req, res) => {
-	console.log(req.params)
+	// console.log(req.params)
 
 	Project.getProjectActions(req.params.id)
 	.then(action => {
+		// console.log(action)
 		res.status(200).json(action);
 	})
 	.catch(err => {
 		res.status(500).json({ error: "The server doesn't like what your doing!" })
 	})
 })
+
+function validateId(req, res, next) {
+	console.log(req.params.id);
+
+	Project.getProjectActions(req.params.id)
+		.then(proj => {
+			console.log(proj)
+		if(proj.length > 0) {
+			console.log('shikes')
+			next();
+		} else {
+			res.status(400).json({ error: 'Error, error, error Will Robinson!' })
+		}
+	})
+	.catch(err => {
+		next();
+	})
+	
+}
 
 module.exports = router;
